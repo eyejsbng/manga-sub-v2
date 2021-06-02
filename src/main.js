1fac55fd64fb2ff4164e31efb86bfb96db812662
jaysanity29 <subangjay@gmail.com>
2021-06-02 13:35:38 +0800

    initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import Vue from 'vue'
+import App from './App.vue'
+import VueLazyLoad from 'vue-lazyload'
+import VueRouter from 'vue-router'
+import router from './router'
+
+Vue.use(VueLazyLoad, {
+  error: './assets/blank.png'
+})
+Vue.use(VueRouter)
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
